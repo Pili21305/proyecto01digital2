867ee669afa8d0cfeb8f0c867add9dbae0e28212
Pili21305 <134110992+Pili21305@users.noreply.github.com>
2023-08-30 15:35:19 -0600

    Config.h

---
+++ Config.h
@@ -0,0 +1,10 @@
+#define IO_USERNAME  "Pili21305"
+#define IO_KEY       "aio_OInK32iQrtshkFoAny3r0UyyY3up"
+
+#define WIFI_SSID "iPhonePili"
+#define WIFI_PASS "papasfritas123"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
